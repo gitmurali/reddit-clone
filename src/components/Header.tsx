d35c6f52001478871a6f458adb1ba3d0bc2fc5b0
gitmurali <murali.nutalapatii@gmail.com>
2022-04-15 13:03:39 +0100

    add appbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,44 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import { useUser } from "../context/AuthContext";
+import RedditIcon from "@mui/icons-material/Reddit";
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useRouter } from "next/router";
 
 type Props = {};
 
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+// }));
+
 export default function Header({}: Props) {
+  //   const classes = useStyles();
   const { user } = useUser();
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" color="inherit">
         <Toolbar>
           <IconButton
             size="large"
@@ -24,25 +50,56 @@ export default function Header({}: Props) {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <RedditIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
+            Reddit clone
           </Typography>
           {user && (
             <div>
               <IconButton
-                size="large"
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
+                onClick={handleMenu}
                 color="inherit"
               >
                 <AccountCircle />
               </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
             </div>
           )}
-          {!user && <div>login</div>}
+          {!user && (
+            <>
+              <Button variant="outlined" onClick={() => router.push("/login")}>
+                Login
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => router.push("/signup")}
+              >
+                Sign up
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
